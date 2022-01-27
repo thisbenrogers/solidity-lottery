97f47baed6a245fc72fba0edac85f9b3e6affec1
BenR <20133902+thisbenrogers@users.noreply.github.com>
2022-01-27 13:04:12 -0600

    Installs dotenv package
    Configures dotenv to provide process.env variables for leaving sensitive information out of commits. Also updates .gitignore to ignore .env file

--- deploy.js
+++ deploy.js
@@ -2,10 +2,12 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
+require('dotenv').config;
+
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
+  process.env.MNEMONIC,
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
+  process.env.ENDPOINT
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
