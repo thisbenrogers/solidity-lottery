ce505cee9f5a86932e53b16478fba97651c95785
BenR <20133902+thisbenrogers@users.noreply.github.com>
2022-01-27 13:06:56 -0600

    Adds missing parens to call .configure

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
-require('dotenv').config;
+require('dotenv').config();
 
 const provider = new HDWalletProvider(
   process.env.MNEMONIC,
